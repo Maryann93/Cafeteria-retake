2e63e8f240f69619284ded1c0eb7bbacdd992cfe
Maryann93 <126924101+Maryann93@users.noreply.github.com>
2023-04-09 10:28:13 -0500

    modified:   cafeteria.html 	modified:   styles/styles.css 	modified:   styles/styles.js

--- styles/styles.js
+++ styles/styles.js
@@ -30,4 +30,47 @@ var texto = document.getElementById('Mensaje').value;
         alert("El textarea no puede estar vacío")
 
 }
+
+//  seleccionamos los dos elementos que serán clickables
+
+/*const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+/* 
+  cada ves que se haga click en el botón 
+  agrega y quita las clases necesarias 
+  para que el menú se muestre.
+*/
+/*toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
+});
+
+/* 
+  Cuándo se haga click fuera del contenedor de enlaces 
+  el menú debe esconderse.
+*/
+
+/*navWrapper.addEventListener("click", e => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
+  }
+});
+*/
 }}}
+
+const toggleButton = document.getElementById('button-menu')
+const navWrapper = document.getElementById('nav')
+
+toggleButton.addEventListener('click',() => {
+  toggleButton.classList.toggle('close')
+  navWrapper.classList.toggle('show')
+})
+
+navWrapper.addEventListener('click',e => {
+  if(e.target.id === 'nav'){
+    navWrapper.classList.remove('show')
+    toggleButton.classList.remove('close')
+  }
+})
